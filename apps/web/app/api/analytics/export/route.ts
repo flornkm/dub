2ccdd1a0bc969a7a003818144d1e2f7f27415338
flooorianflo <florian.kiem@hfg.design>
2024-03-27 18:39:05 +0100

    feat: promise.all + restructure code

--- apps/web/app/api/analytics/export/route.ts
+++ apps/web/app/api/analytics/export/route.ts
@@ -1,4 +1,4 @@
-import { VALID_TINYBIRD_ENDPOINTS, getAnalytics } from "@/lib/analytics";
+import { getAnalytics } from "@/lib/analytics";
 import { DubApiError } from "@/lib/api/errors";
 import { withAuth } from "@/lib/auth";
 import { getDomainViaEdge } from "@/lib/planetscale";
@@ -53,76 +53,26 @@ export const GET = withAuth(
         : null;
 
     const zip = new JSZip();
+    const promises = [] as Promise<void>[];
 
     for (const endpoint of exportableEndpoints) {
-      let response;
+      let promise: Promise<void>;
+
       if (endpoint === "top_links") {
-        const data = await getAnalytics({
-          workspaceId: workspace.id,
-          endpoint: "top_links",
-          ...parsedParams,
-        });
-
-        if (!data || data.length === 0) continue;
-
-        const [links, domains] = await Promise.all([
-          prisma.link.findMany({
-            where: {
-              projectId: workspace.id,
-              id: { in: data.map(({ link }) => link) },
-            },
-            select: { id: true, domain: true, key: true, url: true },
-          }),
-          prisma.domain.findMany({
-            where: {
-              projectId: workspace.id,
-              id: { in: data.map(({ link }) => link) },
-            },
-            select: { id: true, slug: true, target: true },
-          }),
-        ]);
-
-        const allLinks = [
-          ...links.map((link) => ({
-            linkId: link.id,
-            shortLink: linkConstructor({
-              domain: link.domain,
-              key: link.key,
-              pretty: true,
-            }),
-            url: link.url,
-          })),
-          ...domains.map((domain) => ({
-            linkId: domain.id,
-            shortLink: linkConstructor({
-              domain: domain.slug,
-              pretty: true,
-            }),
-            url: domain.target || "",
-          })),
-        ];
-
-        const topLinks = data.map((d) => ({
-          ...allLinks.find((l) => l.linkId === d.link),
-          clicks: d.clicks,
-        }));
-
-        const csvData = convertToCSV(topLinks);
-        zip.file(`${endpoint}.csv`, csvData);
+        promise = getTopLinksData(workspace, parsedParams, zip);
       } else {
-        response = await getAnalytics({
-          workspaceId: workspace.id,
-          ...(linkId && { linkId }),
-          endpoint: endpoint as (typeof VALID_TINYBIRD_ENDPOINTS)[number],
-          ...parsedParams,
-        });
-
-        if (!response || response.length === 0) continue;
-
-        const csvData = convertToCSV(response);
-        zip.file(`${endpoint}.csv`, csvData);
+        promise = getAnalyticsData(
+          workspace,
+          linkId,
+          endpoint,
+          parsedParams,
+          zip,
+        );
       }
+      promises.push(promise);
     }
+
+    await Promise.all(promises);
 
     const zipData = await zip.generateAsync({ type: "nodebuffer" });
 
@@ -137,3 +87,78 @@ export const GET = withAuth(
     needNotExceededClicks: true,
   },
 );
+
+async function getTopLinksData(workspace, parsedParams, zip) {
+  const data = await getAnalytics({
+    workspaceId: workspace.id,
+    endpoint: "top_links",
+    ...parsedParams,
+  });
+
+  if (!data || data.length === 0) return;
+
+  const [links, domains] = await Promise.all([
+    prisma.link.findMany({
+      where: {
+        projectId: workspace.id,
+        id: { in: data.map(({ link }) => link) },
+      },
+      select: { id: true, domain: true, key: true, url: true },
+    }),
+    prisma.domain.findMany({
+      where: {
+        projectId: workspace.id,
+        id: { in: data.map(({ link }) => link) },
+      },
+      select: { id: true, slug: true, target: true },
+    }),
+  ]);
+
+  const allLinks = [
+    ...links.map((link) => ({
+      linkId: link.id,
+      shortLink: linkConstructor({
+        domain: link.domain,
+        key: link.key,
+        pretty: true,
+      }),
+      url: link.url,
+    })),
+    ...domains.map((domain) => ({
+      linkId: domain.id,
+      shortLink: linkConstructor({
+        domain: domain.slug,
+        pretty: true,
+      }),
+      url: domain.target || "",
+    })),
+  ];
+
+  const topLinks = data.map((d) => ({
+    ...allLinks.find((l) => l.linkId === d.link),
+    clicks: d.clicks,
+  }));
+
+  const csvData = convertToCSV(topLinks);
+  zip.file(`top_links.csv`, csvData);
+}
+
+async function getAnalyticsData(
+  workspace,
+  linkId,
+  endpoint,
+  parsedParams,
+  zip,
+) {
+  const response = await getAnalytics({
+    workspaceId: workspace.id,
+    ...(linkId && { linkId }),
+    endpoint,
+    ...parsedParams,
+  });
+
+  if (!response || response.length === 0) return;
+
+  const csvData = convertToCSV(response);
+  zip.file(`${endpoint}.csv`, csvData);
+}
